c089ac8ffbbc1c48cd99110720c43a7c7c921e3f
Stephanie Sison <sjsison27@icloud.com>
2022-05-18 23:16:29 -0700

    commit tree 6e637d0de51c990df74c9dcfe0a1938e8f7daf54

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide() {
+    let a = 2000
+    let b = 100
+    return a/b
+}
+
+function square() {
+    let x = 2 
+    const multiply = x => x * x;
+    return x * x;
+}
+
+function add() {
+    let a = 3
+    let b = 4
+    const add = (a, b) => a + b;
+    return a + b;
+}
